2d4ec620e5a84df77b6d7c3047ac8c9c015ec716
Lev <lev.l.baranov@gmail.com>
2020-05-03 18:35:11 +0500

    Констурктор Memory, метод get_data()

--- classificator.py
+++ classificator.py
@@ -1,4 +1,5 @@
-from sys import stderr 
+from sys import stderr
+from elasticsearch import Elasticsearch
 
 class ArgumentException(Exception):
     pass
@@ -22,20 +23,44 @@ class Classificator:
 class Memory:
     'Хранит результаты, а также предоставляет информацию для обучения.'   
 
-    def get_data():
-        pass
+    def __init__(self, address):
+        self.address = address
+        self.es = self._connect_elastic(address)
+
+    def __repr__(self):
+        return "Memory('%s')" % (self.address)
+
+    def get_data(self):
+        query = {"query": {"match_all": {}}}
+        index_name = 'category'
+        results = self.es.search(index=index_name, body=query, size=999)
+        data = dict()
+        for hit in results['hits']['hits']:
+            category = hit['_source']['category']
+            string = hit['_source']['string']
+            if data.get(category) is not None:
+                data[category].append(string)
+            else:
+                data[category] = [string]
+        return data
 
     def remember(request, respone):
         if(request and respone):
             pass
         else:
             raise ArgumentException()
+    
+    def _connect_elastic(self, address):
+        _es = Elasticsearch([address])
+        if _es.ping():
+            #print('Connect Established')
+            return _es
 
-   
+
 if __name__ == '__main__':
     try:
         classificator = Classificator
-        memory = Memory
+        memory = Memory('http://localhost:9200')
         classificator.learn(memory.get_data())
         request = 'Не показывает канал матч';
         respone = classificator.define_category(request)
