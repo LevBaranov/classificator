7bddab73936701f46ffb3a9bd35a1b163ee820b7
Lev <lev.l.baranov@gmail.com>
2020-05-16 23:13:49 +0500

    Реализация learn()

--- classificator.py
+++ classificator.py
@@ -1,5 +1,12 @@
 from sys import stderr
+import numpy as np
 import elasticsearch 
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.tools.customxml import NetworkWriter
+from pybrain.structure import TanhLayer, SoftmaxLayer, LinearLayer
+
 
 class ArgumentException(Exception):
     pass
@@ -7,18 +14,57 @@ class ArgumentException(Exception):
 class Classificator:
 
 
-    def learn(data):
+    data_for_training = []
+    
+    def learn(self, data, all_words):
         if data:
-            pass
+            categories = list(data.keys())
+            for category in data:
+                for string in data[category]:
+                    out_layer = [0] * len(categories)
+                    in_layer = self._tokenization(string, all_words)
+                    out_layer[categories.index(category)] = 1
+                    self.data_for_training.append([in_layer, out_layer])
+            self.data_for_training = np.array(self.data_for_training)
+            self.net = buildNetwork(
+                len(self.data_for_training[:, 0][0]), 
+                5, 
+                len(self.data_for_training[:, 1][0]), 
+                hiddenclass=LinearLayer, 
+                recurrent=False
+                )
+            ds = SupervisedDataSet(
+                len(self.data_for_training[:, 0][0]), 
+                len(self.data_for_training[:, 1][0])
+                )
+            i = 0
+            for inp in self.data_for_training[:, 0]:
+                ds.addSample(inp, self.data_for_training[:, 1][i])
+                i += 1
+
+            trainer = BackpropTrainer(self.net, ds, learningrate=0.05)
+            trainer.trainEpochs(500)
+            NetworkWriter.writeToFile(self.net, 'net.xml')
         else:
             raise ArgumentException()
 
-    def define_category(request):
+    def define_category(self, request):
         if request:
             return request
         else:
             raise ArgumentException()
 
+    def _tokenization(self, string, all_words):
+        token_string = []
+        list_words = string.lower().split(' ')
+        for word in all_words:
+            if word in list_words:
+                token_string.append(1)
+            else:
+                token_string.append(0)
+        return token_string
+
+    
 
 class Memory:
     'Хранит результаты, а также предоставляет информацию для обучения.'   
@@ -27,7 +73,7 @@ class Memory:
 
     all_words = set()
 
-    category_words = dict() 
+    data_templates = dict() 
 
     def __init__(self, address):
         self.address = address
@@ -48,12 +94,12 @@ class Memory:
             words = {w.lower() for w in string.split(' ')}
             self.all_words.update(words)
 
-            if self.category_words.get(category) is not None:
-                self.category_words[category].update(words)
+            if self.data_templates.get(category) is not None:
+                self.data_templates[category].append(string)
             else:
-                self.category_words[category] = set(words)
+                self.data_templates[category] = [string]
 
-        return self.category_words
+        return self.data_templates
 
     def remember(self, request, response):
         if request and response:
@@ -71,17 +117,16 @@ class Memory:
 
 if __name__ == '__main__':
     try:
-        classificator = Classificator
+        classificator = Classificator()
         memory = Memory('http://localhost:9200')
         data = memory.get_data()
-        print(memory.category)
-        print(memory.all_words)
+        all_words = memory.all_words
         print(data)
-        classificator.learn(memory.get_data())
+        print(classificator.learn(data, all_words))
         request = 'Не показывает канал матч';
         response = classificator.define_category(request)
         assert response == request  # Поменяю когда будет готов define_category()
         memory.remember(request, response)
         print(response)
     except Exception as e:
-        print(type(e), file=stderr)
+        print(e, file=stderr)
