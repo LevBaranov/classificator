d180dbe4fc9982ec6287d32104623452f09d875e
Lev <lev.l.baranov@gmail.com>
2020-05-17 22:59:10 +0500

    Создан Request (перенёс токенизацию). Реализация define_category. Изменил структуру сети

--- classificator.py
+++ classificator.py
@@ -2,9 +2,9 @@ from sys import stderr
 import numpy as np
 import elasticsearch 
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import SupervisedDataSet
+from pybrain.datasets import ClassificationDataSet
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkWriter, NetworkReader
 from pybrain.structure import TanhLayer, SoftmaxLayer, LinearLayer
 
 
@@ -16,64 +16,47 @@ class Classificator:
 
     data_for_training = []
     
-    def learn(self, data, all_words):
-        if data:
+    def learn(self):
+        try:
+            mem = Memory('http://localhost:9200')
+            data = mem.get_data()
             categories = list(data.keys())
             for category in data:
-                for string in data[category]:
-                    out_layer = [0] * len(categories)
-                    in_layer = self._tokenization(string, all_words)
-                    out_layer[categories.index(category)] = 1
-                    self.data_for_training.append([in_layer, out_layer])
-            self.data_for_training = np.array(self.data_for_training)
-            self.net = buildNetwork(
-                len(self.data_for_training[:, 0][0]), 
-                5, 
-                len(self.data_for_training[:, 1][0]), 
-                hiddenclass=LinearLayer, 
+                for req in data[category]:
+                    self.data_for_training.append([req.token, [categories.index(category)]])
+
+            net = buildNetwork(
+                len(self.data_for_training[0][0]), 
+                15, 
+                1, 
+                hiddenclass=SoftmaxLayer, 
                 recurrent=False
                 )
-            ds = SupervisedDataSet(
-                len(self.data_for_training[:, 0][0]), 
-                len(self.data_for_training[:, 1][0])
+            ds = ClassificationDataSet(
+                len(self.data_for_training[0][0]), 
+                nb_classes=len(categories),
+                class_labels=categories
                 )
-            i = 0
-            for inp in self.data_for_training[:, 0]:
-                ds.addSample(inp, self.data_for_training[:, 1][i])
-                i += 1
+            for data in self.data_for_training:
+                ds.addSample(data[0], data[1])
 
-            trainer = BackpropTrainer(self.net, ds, learningrate=0.05)
-            trainer.trainEpochs(500)
-            NetworkWriter.writeToFile(self.net, 'net.xml')
-        else:
-            raise ArgumentException()
+            trainer = BackpropTrainer(net, ds, momentum=0.1, learningrate=0.01, verbose=True)
+            trainer.trainUntilConvergence(maxEpochs=500)
+            NetworkWriter.writeToFile(net, 'net.xml')
+        except Exception as e:
+            raise e
+        return net
 
     def define_category(self, request):
-        if request:
-            return request
+        if type(request) == Request:
+            net = NetworkReader.readFrom('net.xml')
+            return net.activate(request.token)
         else:
             raise ArgumentException()
 
-    def _tokenization(self, string, all_words):
-        token_string = []
-        list_words = string.lower().split(' ')
-        for word in all_words:
-            if word in list_words:
-                token_string.append(1)
-            else:
-                token_string.append(0)
-        return token_string
-
-    
 
 class Memory:
     'Хранит результаты, а также предоставляет информацию для обучения.'   
-
-    category = []
-
-    all_words = set()
-
-    data_templates = dict() 
 
     def __init__(self, address):
         self.address = address
@@ -83,13 +66,14 @@ class Memory:
         return "Memory('%s')" % (self.address)
 
     def get_data(self):
+        self.all_words = set()
+        self.data_templates = dict()
         query = {"query": {"match_all": {}}}
         index_name = 'category'
         results = self.es.search(index=index_name, body=query, size=999)
 
         for hit in results['hits']['hits']:
             category = hit['_source']['category']
-            self.category.append(category)
             string = hit['_source']['string']
             words = {w.lower() for w in string.split(' ')}
             self.all_words.update(words)
@@ -98,7 +82,11 @@ class Memory:
                 self.data_templates[category].append(string)
             else:
                 self.data_templates[category] = [string]
-
+        for cat in self.data_templates:
+            for i, val in enumerate(self.data_templates[cat]):
+                self.data_templates[cat][i] = Request(
+                    val, self.all_words
+                    )
         return self.data_templates
 
     def remember(self, request, response):
@@ -114,19 +102,42 @@ class Memory:
         except Exception as e:
             raise e
 
+class Request:
+    'Класс который будет обрабатывать запрос и предоставлять вместо слов токен'
+
+    def __init__(self, string, dictionary):
+        self.text_request, self.dictionary = string, dictionary
+        self.token = self._tokenization(string, dictionary)
+
+    def __repr__(self):
+        return "Request('%s', %s)" % (self.text_request, self.dictionary)
+
+    def _tokenization(self, string, dictionary):
+        token_string = []
+        list_words = string.lower().split(' ')
+        for word in dictionary:
+            if word in list_words:
+                token_string.append(1)
+            else:
+                token_string.append(0)
+        return token_string
+
 
 if __name__ == '__main__':
-    try:
+    #try:
         classificator = Classificator()
         memory = Memory('http://localhost:9200')
         data = memory.get_data()
         all_words = memory.all_words
-        print(data)
-        print(classificator.learn(data, all_words))
-        request = 'Не показывает канал матч';
+        category = list(data.keys())
+        #print(data)
+        print(classificator.learn())
+        request = Request('нет изображение на камере', all_words);
         response = classificator.define_category(request)
-        assert response == request  # Поменяю когда будет готов define_category()
-        memory.remember(request, response)
+        #assert response == request  # Поменяю когда будет готов define_category()
+        #memory.remember(request, response)
+        print(category)
         print(response)
-    except Exception as e:
-        print(e, file=stderr)
+        print(category[int(response)])
+    #except Exception as e:
+    #    print(e, file=stderr)
